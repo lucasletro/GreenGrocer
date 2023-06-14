4003661b4ee2cf9be3d127c9a6c07f00b94ac17d
Lucas Letro <lucasletro@hotmail.com>
2023-06-14 18:31:14 -0300

    Criando botão entrar e esqueci a senha.

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -30,19 +30,51 @@ class SignInScreen extends StatelessWidget {
                   )
                 ),
 
-                child: const Column(
+                child: Column(
+                  //esticar os componentes no eixo cruzado(Botao entrar...)
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    //email
-                    CustomTextField(
+                    //EMAIL
+                    const CustomTextField(
                       icon: Icons.email,
                       label: "Email",
                     ),
 
-                    //senha
-                    CustomTextField(
+                    //SENHA
+                    const CustomTextField(
                       icon: Icons.lock,
                       label: "Senha",
                       isSecret: true,
+                    ),
+
+                    //BOTAO ENTRAR
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          backgroundColor: Colors.green
+                        ),
+                          onPressed: (){},
+                          child: const Text("Entrar", style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white
+                          ),),
+                      ),
+                    ),
+
+                    //BOTAO ESQUECI A SENHA
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: (){},
+                        child: const Text(
+                          "Esqueceu a senha?",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
                     ),
 
                   ],
