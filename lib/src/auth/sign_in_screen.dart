9a3a37edbd6bd6c2d7154ab708ff529ebc48454d
Lucas Letro <lucasletro@hotmail.com>
2023-06-15 12:57:01 -0300

    Adicionando nome do app e animação das categorias.

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 
@@ -14,10 +15,60 @@ class SignInScreen extends StatelessWidget {
       body: Column(
         children: [
 
+          //NOME DO APP
           Expanded(
-              child: Container(
-            color: Colors.red,
-          )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  //NOME DO APP
+                  const Text.rich(TextSpan(
+                    style: TextStyle(
+                      fontSize: 40,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: "Green",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "grocer",
+                        style: TextStyle(
+                          color: Colors.red,
+
+                        )
+                      ),
+                    ],
+                  )),
+
+                  //CATEGORIAS
+                  SizedBox(
+                    height: 30,
+                    child: DefaultTextStyle(
+                      style: const TextStyle(
+                        fontSize: 25,
+                      ),
+                      child: AnimatedTextKit(
+                        pause: Duration.zero,
+                        repeatForever: true,
+                        animatedTexts: [
+                          FadeAnimatedText("Frutas"),
+                          FadeAnimatedText("Verduras"),
+                          FadeAnimatedText("Legumes"),
+                          FadeAnimatedText("Carnes"),
+                          FadeAnimatedText("Cereais"),
+                          FadeAnimatedText("Laticíneos"),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+          ),
 
           //CONTAINER BRANCO COM BORDAS ARREDONDADAS
           Container(
