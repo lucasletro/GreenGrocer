8333bb3c12371cbbdb0e6bf440634d46048b7bfe
Lucas Letro <lucasletro@hotmail.com>
2023-06-15 12:35:08 -0300

    Criando botão de cadastro.

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -20,66 +20,113 @@ class SignInScreen extends StatelessWidget {
           )),
 
           //CONTAINER BRANCO COM BORDAS ARREDONDADAS
-          Expanded(
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(45),
-                  )
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(45),
+              )
+            ),
+
+            child: Column(
+              //esticar os componentes no eixo cruzado(Botao entrar...)
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                //EMAIL
+                const CustomTextField(
+                  icon: Icons.email,
+                  label: "Email",
                 ),
 
-                child: Column(
-                  //esticar os componentes no eixo cruzado(Botao entrar...)
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    //EMAIL
-                    const CustomTextField(
-                      icon: Icons.email,
-                      label: "Email",
-                    ),
-
-                    //SENHA
-                    const CustomTextField(
-                      icon: Icons.lock,
-                      label: "Senha",
-                      isSecret: true,
-                    ),
-
-                    //BOTAO ENTRAR
-                    SizedBox(
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                          backgroundColor: Colors.green
-                        ),
-                          onPressed: (){},
-                          child: const Text("Entrar", style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white
-                          ),),
-                      ),
-                    ),
-
-                    //BOTAO ESQUECI A SENHA
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: (){},
-                        child: const Text(
-                          "Esqueceu a senha?",
-                          style: TextStyle(color: Colors.red),
-                        ),
-                      ),
-                    ),
-
-                  ],
+                //SENHA
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: "Senha",
+                  isSecret: true,
                 ),
-          )),
+
+                //BOTAO ENTRAR
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      backgroundColor: Colors.green
+                    ),
+                      onPressed: (){},
+                      child: const Text("Entrar", style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white
+                      ),),
+                  ),
+                ),
+
+                //BOTAO ESQUECI A SENHA
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: (){},
+                    child: const Text(
+                      "Esqueceu a senha?",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ),
+
+                //DIVISOR
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text("Ou"),
+                      ),
+
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                //BOTAO DE NOVO USUARIO
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      side: const BorderSide(
+                        width: 2,
+                        color: Colors.green
+                      ),
+                    ),
+                    onPressed: (){},
+                    child: const Text("Criar conta", style: TextStyle(
+                      fontSize: 18,
+                    ),),
+                  ),
+                ),
+
+
+              ],
+            ),
+          ),
 
         ],
       ),
